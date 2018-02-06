284f34b9082519418a3f6bcde08b5cd79d74ad44
98Glopes <34631226+98Glopes@users.noreply.github.com>
2018-02-06 17:47:47 -0200

    Update index.py

--- index.py
+++ index.py
@@ -108,4 +108,4 @@ if __name__ == '__main__':
 #	cam  = VideoCamera() # camera
 	app.secret_key = os.urandom(12)
 	port = int(os.environ.get("PORT", 5000))
-	app.run(debug=True)
+	app.run(debug=False, port=port)
