703bb5251aaaa7c9cecb38ce0e624073ccba57f6
98Glopes <34631226+98Glopes@users.noreply.github.com>
2018-02-21 16:25:14 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -113,4 +113,4 @@ if __name__ == '__main__':
 #	cam  = VideoCamera() # camera
 	app.secret_key = os.urandom(12)
 	port = int(os.environ.get("PORT", 5000))
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port=port, debug=True)
